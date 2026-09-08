e9105096fa6e6fa06b8493451b561ec576ea1708
baran <contact@louisbaranzelli.com>
2026-09-08 06:07:04 +0200

    [ESTIMATEUR NON-PARAMERIQUE] feat: ajout d'un estimateur de bande de confiance non parametrique pour une distribution continue

---
+++ quant-statistics/src/main/java/org/statistics/probability/tools/RandomVectorFactory.java
@@ -0,0 +1,21 @@
+package org.statistics.probability.tools;
+
+import org.math.vector.ArrayVector;
+import org.math.vector.Vector;
+
+import java.util.random.RandomGenerator;
+import java.util.random.RandomGeneratorFactory;
+
+public class RandomVectorFactory {
+    /**
+     * Générateur déterministe d'échantillons i.i.d. de la loi Uniforme(0, 1)
+     */
+    public static Vector generateUniformSample(int n, long seed) {
+        RandomGenerator rng = RandomGeneratorFactory.of("L64X128MixRandom").create(seed);
+        double[] data = new double[n];
+        for (int i = 0; i < n; i++) {
+            data[i] = rng.nextDouble();
+        }
+        return new ArrayVector(data);
+    }
+}
